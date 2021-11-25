c2b7850374a3f1220e089a2aa06c54b67467cbbf
Victor Ramon <victorrgds02@gmail.com>
2021-11-25 17:58:59 -0300

    ortografia melhorada

--- 8-ball/Assets/script2.js
+++ 8-ball/Assets/script2.js
@@ -4,7 +4,7 @@
 
 const answersMagicBall = ["Pela angulação do taco, essa é caçapa!","Sem dúvida essa cai!",
 "Hmm... Essa é caçapa!","Tenho minhas dúvidas","Essa a física não permite!","Sim!","Definitivamente!",
-"Pode confiar, é caçapa!","Ih rapaz... Melhor deixa essa para lá...",
+"Pode confiar, é caçapa!","Ih rapaz... Melhor deixar essa para lá...",
 "Para não te comprometer, vou deixar essa em off...","Melhor nem tentar... Essa não cai!",
 "Não vai cair nunca, passa um giz e tenta de novo!","Essa não precisa nem de giz, é certeza!", 
 "A meu ver, essa entra!","Essa entra, certeza!","Quase cai! Passa um giz e tenta de novo!",
